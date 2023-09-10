e8ba694ff4d4ed0e1159273dae26422d5761240c
Santiago Martínez <santiagomartinezc96@gmail.com>
2023-09-10 17:44:36 -0500

    fin tarea 2

--- Tarea_2/pruebita.py
+++ Tarea_2/pruebita.py
@@ -1,113 +1,82 @@
 import numpy as np
-import zipfile
-import tarfile
 import os
-from collections import defaultdict
-import nltk
-from nltk.util import ngrams
-from tqdm import tqdm 
-import xml.etree.ElementTree as ET
 import re
-from nltk.tokenize import sent_tokenize, word_tokenize
 import random
-from collections import Counter
 
-# Specify the folder name
 n_grams_folder = "n_grams"
-
-# Check if the folder already exists
-if not os.path.exists(n_grams_folder):
-    # If it doesn't exist, create it
-    os.mkdir(n_grams_folder)
-    print(f"Folder '{n_grams_folder}' created successfully.")
-else:
-    print(f"Folder '{n_grams_folder}' already exists.")
-
-
-# Define a function to calculate Laplace-smoothed probabilities for n-grams
-def laplace_smoothing(ngram_counts, total_tokens, vocab_size, alpha=1.0):
-    smoothed_probabilities = {}
-    for ngram, count in ngram_counts.items():
-        smoothed_prob = (count + alpha) / (total_tokens + alpha * vocab_size)
-        smoothed_probabilities[ngram] = smoothed_prob
-
-    return smoothed_probabilities
-
-# Define a function to generate n-grams from a list of tokens
-def generate_ngrams(tokens, n):
-    ngrams = []
-    for i in range(len(tokens) - n + 1):
-        ngram = tuple(tokens[i:i + n])
-        ngrams.append(ngram)
-    return ngrams
-
-# Define a function to build and save N-gram models with Laplace smoothing
-def build_and_save_ngram_model(sentences, n, output_file):
-    breakpoint()
-    # Tokenize the sentences into words
-    tokens = " ".join(sentences).split()
-
-    # Generate n-grams
-    ngrams = generate_ngrams(tokens, n)
-
-    # Calculate n-gram counts
-    ngram_counts = Counter(ngrams)
-
-    # Calculate vocabulary size
-    vocab_size = len(set(tokens))
-
-    # Calculate total number of tokens
-    total_tokens = len(tokens)
-
-    # Apply Laplace smoothing to calculate smoothed probabilities
-    smoothed_probabilities = laplace_smoothing(ngram_counts, total_tokens, vocab_size)
-
-    # Save the smoothed probabilities to the output file
-    with open(output_file, "w", encoding="utf-8") as file:
-        for ngram, prob in smoothed_probabilities.items():
-            ngram_str = " ".join(ngram)
-            file.write(f"{ngram_str}\t{prob}\n")
-
-# Download NLTK data for tokenization
-nltk.download('punkt')
-
 group_code = "group_code" 
-# Paths to preprocessed training files
-preprocessed_training_file_20N = f"20N_{group_code}_training.txt"
-preprocessed_training_file_BAC = f"BAC_{group_code}_training.txt"
 
-# Load preprocessed training data from the files
-with open(preprocessed_training_file_20N, "r", encoding="utf-8") as file:
-    train_sentences_20N = file.read()
+def custom_tokenizer(text):
+    # Use regular expression to split on spaces while preserving '<s>' and '</s>' as separate tokens
+    tokens = re.split(r'(\s|<s>|</s>)', text)
+    
+    # Remove empty tokens and tokens containing only special characters
+    tokens = [token for token in tokens if token.strip() and not re.match(r'^[^a-zA-Z0-9]+$', token)]
+    
+    # Remove trailing '>' characters from tokens like "the>"
+    tokens = [re.sub(r'(.*[^>])>', r'\1', token) if token not in ['<s>','</s>', '<UNK>'] else token for token in tokens]
+    
+    return tokens
 
-with open(preprocessed_training_file_BAC, "r", encoding="utf-8") as file:
-    train_sentences_BAC = file.read()
+# Load the n-gram model
+def load_ngram_model(model_file):
+    ngram_model = {}
+    with open(model_file, "r", encoding="utf-8") as file:
+        for line in file:
+            ngram, probability = line.strip().split("\t")
+            ngram_model[ngram] = float(probability)
+    return ngram_model
 
-# Paths to training files
-training_file_20N = f"20N_{group_code}_training.txt"
-training_file_BAC = f"BAC_{group_code}_training.txt"
+# Function to generate the next word based on the n-gram model
+def generate_next_word(ngram_model, current_words):
+    candidates = []
+    probabilities = []
+    for key in ngram_model.keys():
+        if key.startswith(current_words):
+            candidates.append(key.split(" ")[-1])
+            probabilities.append(ngram_model.get(key))
+    if not candidates:
+        # rwg = random.choice(list(ngram_model.keys())) 
+        return None # No candidates found, returns random word in vocab
+    
+    # Calculate probabilities for each candidate word
+    # probabilities = [ngram_model.get(f"{current_words} {candidate}", 0.0) for candidate in candidates]
 
-# Paths to output files
-output_file_20N_unigrams = f"20N_{group_code}_unigrams.txt"
-output_file_20N_bigrams = f"20N_{group_code}_bigrams.txt"
-output_file_20N_trigrams = f"20N_{group_code}_trigrams.txt"
-output_file_BAC_unigrams = f"BAC_{group_code}_unigrams.txt"
-output_file_BAC_bigrams = f"BAC_{group_code}_bigrams.txt"
-output_file_BAC_trigrams = f"BAC_{group_code}_trigrams.txt"
+    # Normalize the probabilities to ensure they sum to 1
+    total_probability = sum(probabilities)
 
-# Build and save N-gram models with Laplace smoothing
-build_and_save_ngram_model(train_sentences_20N, 1, os.path.join(n_grams_folder,output_file_20N_unigrams))
-print("1-gram models with Laplace smoothing generated and saved.")
-build_and_save_ngram_model(train_sentences_20N, 2, os.path.join(n_grams_folder,output_file_20N_bigrams))
-print("2-gram models with Laplace smoothing generated and saved.")
-build_and_save_ngram_model(train_sentences_20N, 3, os.path.join(n_grams_folder,output_file_20N_trigrams))
-print("3-gram models with Laplace smoothing generated and saved.")
-build_and_save_ngram_model(train_sentences_BAC, 1, os.path.join(n_grams_folder,output_file_BAC_unigrams))
-print("1-gram models with Laplace smoothing generated and saved.")
-build_and_save_ngram_model(train_sentences_BAC, 2, os.path.join(n_grams_folder,output_file_BAC_bigrams))
-print("2-gram models with Laplace smoothing generated and saved.")
-build_and_save_ngram_model(train_sentences_BAC, 3, os.path.join(n_grams_folder,output_file_BAC_trigrams))
-print("3-gram models with Laplace smoothing generated and saved.")
+    probabilities = [prob / total_probability for prob in probabilities]
 
-# Print confirmation message
-print("N-gram models with Laplace smoothing generated and saved.")
+    # Use the calculated and normalized probabilities to randomly select the next word
+    next_word = np.random.choice(candidates, p=probabilities)
+
+    return next_word
+
+# Function to generate a sentence
+def generate_sentence(start_word, ngram_model, n, max_length=50):
+    sentence = [start_word.lower()]
+    
+    while len(sentence) < max_length:
+        current_words = " ".join(sentence[-(n-1):])
+        next_word = generate_next_word(ngram_model, current_words)
+        if next_word is None:
+            break
+        
+        sentence.append(next_word)
+        
+        if next_word == "</s>":
+            break
+    
+    return " ".join(sentence)
+
+# Choose the n-gram model to use (e.g., trigrams)
+n_gram_model_file = os.path.join(n_grams_folder, f"BAC_{group_code}_trigrams.txt")
+# Load the selected n-gram model
+ngram_model = load_ngram_model(n_gram_model_file)
+
+# Example usage:
+start_word = "you"  # Replace with your desired start word
+generated_sentence = generate_sentence(start_word, ngram_model, n=3)
+
+print("Generated Sentence:")
+print(generated_sentence)
